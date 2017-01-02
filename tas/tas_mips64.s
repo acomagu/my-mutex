8ddc45b7f0ad15db79fd42ae2613f04f852bcc5a
Yuki Ito <acomagu@gmail.com>
2017-01-02 19:47:20 +0900

    [Fix] bug not wait

--- tas/tas_mips64.s
+++ tas/tas_mips64.s
@@ -4,12 +4,15 @@
 #define SCV(base, rt)	WORD	$((074<<26)|((base)<<21)|((rt)<<16))
 #define SYNC WORD $0xf
 
-TEXT ·testAndSet(SB),$0
+TEXT ·testAndSet(SB),$0-24
 	MOVV	lock+0(FP), R2
+	MOVW	v+8(FP), R4
 	SYNC
+tas_again:
+	MOVV	R4, R3
 	LL(2, 1)	// R1 = *R2
-	MOVW	v+8(FP), R3
 	SC(2, 3)	// *R2 = R3
-	MOVW	R1, ret+8(FP)
+	BEQ	R3, tas_again
+	MOVW	R1, ret+16(FP)
 	SYNC
 	RET
